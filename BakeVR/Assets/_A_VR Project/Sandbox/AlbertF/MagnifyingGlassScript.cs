f6fc009dfe00ab1d4bc75c7f49919dcd8f92fcfe
Albert <albert.faro@enti.cat>
2022-03-02 19:08:40 +0100

    MagnifyingGlass

--- BakeVR/Assets/_A_VR Project/Sandbox/AlbertF/MagnifyingGlassScript.cs
+++ BakeVR/Assets/_A_VR Project/Sandbox/AlbertF/MagnifyingGlassScript.cs
@@ -7,16 +7,27 @@ using UnityEngine;
 public class MagnifyingGlassScript : MonoBehaviour
 {
 
+
     public GameObject cam;
-    
+    public GameObject camdisplay;
+
+
+    private void Start()
+    {
+        cam.SetActive(false);
+        camdisplay.SetActive(false);
+    }
+
     public void activateCam()
     {
         cam.SetActive(true);
+        camdisplay.SetActive(true);
     }
 
     public void deactivateCam()
     {
         cam.SetActive(false);
+        camdisplay.SetActive(false);
     }
 
 }
