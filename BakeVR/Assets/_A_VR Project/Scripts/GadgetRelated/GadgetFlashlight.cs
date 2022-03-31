8c5d6cfa719f0b55e2d89e18af0627a875bb4426
Albert <albert.faro@enti.cat>
2022-03-31 15:02:54 +0200

    Phantom Voice

--- BakeVR/Assets/_A_VR Project/Scripts/GadgetRelated/GadgetFlashlight.cs
+++ BakeVR/Assets/_A_VR Project/Scripts/GadgetRelated/GadgetFlashlight.cs
@@ -6,17 +6,22 @@ public class GadgetFlashlight : MonoBehaviour
 {
     [SerializeField] private GameObject spotLight;
     [SerializeField] private GameObject lightCollider;
-    
+    [SerializeField] private AudioSource audioSrc;
     // Start is called before the first frame update
     private void Awake()
     {
         TurnOff();
+        audioSrc = GetComponent<AudioSource>();
+        
     }
 
     public void TurnOn()
     {
         spotLight.SetActive(true);
         lightCollider.SetActive(true);
+        audioSrc.Play();
+
+
     }
 
     public void TurnOff()
